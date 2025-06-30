e01ffd98c6121a3e9c8005190d49ba4ccc720c22
Zacharias King <zacking1998@gmail.com>
2025-06-30 18:12:49 +0100

    Delete post functionality

--- index.js
+++ index.js
@@ -1,70 +1,98 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import express from "express";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
 const port = 3000;
 
-  var testBlogPosts = [
-    { id: 1, imageName: 'Holi_FP_B118_1004194166.jpg',title: 'First Blog Post', content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum", date: '2023-10-01', author: 'John Doe' },
-    { id: 2, imageName: 'Outdoor_99D_B022_100365573.jpg', title: 'Second Blog Post', content: 'This is the content of the second blog post.', date: '2024-10-01', author: 'Jane Smith' },
-    { id: 3, imageName: 'smart home_110110747.jpg', title: 'Third Blog Post', content: 'This is the content of the third blog post.', date: '2024-10-23', author: 'Alice Johnson' }
-  ];
+var testBlogPosts = [
+  {
+    id: 1,
+    imageName: "Holi_FP_B118_1004194166.jpg",
+    title: "First Blog Post",
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+    date: "2023-10-01",
+    author: "John Doe",
+  },
+  {
+    id: 2,
+    imageName: "Outdoor_99D_B022_100365573.jpg",
+    title: "Second Blog Post",
+    content: "This is the content of the second blog post.",
+    date: "2024-10-01",
+    author: "Jane Smith",
+  },
+  {
+    id: 3,
+    imageName: "smart home_110110747.jpg",
+    title: "Third Blog Post",
+    content: "This is the content of the third blog post.",
+    date: "2024-10-23",
+    author: "Alice Johnson",
+  },
+];
 
 // Static files middleware
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 //middleware that only parses urlencoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-}); 
-
-app.get('/', (req, res) => {
-  res.render('index.ejs', { blogPosts: testBlogPosts });
+  console.log("Server is running on port 3000");
 });
 
-app.get('/about', (req, res) => {
-  res.render('about.ejs');
+app.get("/", (req, res) => {
+  res.render("index.ejs", { blogPosts: testBlogPosts });
 });
 
-app.get('/contact', (req, res) => {
-  res.render('contact.ejs');
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
 });
 
-app.get('/blog/:id', (req, res) => {
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.get("/blog/:id", (req, res) => {
   const { id } = req.params;
   // Here you would typically fetch the blog post from a database
-  const blogPost = testBlogPosts.find(post => post.id === parseInt(id));
+  const blogPost = testBlogPosts.find((post) => post.id === parseInt(id));
 
-  res.render('blog.ejs', { blogPostForView: blogPost });
+  res.render("blog.ejs", { blogPostForView: blogPost });
 });
 
-
 //Add new blog post via POST endppoint
-app.post('/blog', (req, res) => {
+app.post("/blog", (req, res) => {
   console.log(req.body);
   const { title, content, image } = req.body;
   // Here you would typically save the blog post to a database
   testBlogPosts.push({
     id: testBlogPosts.length + 1,
-    imageName: image ? image.name : 'default.jpg', // Use uploaded image or default
+    imageName: image ? image.name : "default.jpg", // Use uploaded image or default
     title,
     content,
-    date: new Date().toISOString().split('T')[0] // Current date in YYYY-MM-DD format
+    date: new Date().toISOString().split("T")[0], // Current date in YYYY-MM-DD format
   });
   console.log(`New blog post created: ${title} - ${content}`);
-  res.redirect('/');
+  res.redirect("/");
 });
 
 //Update existing blog post via PUT endpoint
-app.put('/blog/:id', (req, res) => {
+app.put("/blog/:id", (req, res) => {
   const { id } = req.params;
   const { title, content } = req.body;
   console.log(`Blog post ${id} updated: ${title} - ${content}`);
   res.redirect(`/blog/${id}`);
+});
+
+app.delete("/blog/:id", (req, res) => {
+  const { id } = req.params;
+  testBlogPosts = testBlogPosts.filter((post) => post.id !== parseInt(id));
+  console.log(`Blog post ${id} deleted`);
+  res.status(200).send({ message: "Post deleted successfully." });
 });
--- public/scripts/eventHandlers.js
+++ public/scripts/eventHandlers.js
@@ -1,23 +1,34 @@
 // Get the modal
 var modal = document.getElementById("addPostModal");
 
-document
-  .getElementById("add-post-button")
-  .addEventListener("click", function () {
-    modal.classList.remove("hide-modal");
-  });
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.classList.add("hide-modal"); 
-  }
-}
-
-
-// Get the <span> element that closes the modal
-document.getElementById("close-modal").addEventListener("click", function () {
-  modal.classList.add("hide-modal"); 
+$("#add-post-button").on("click", function () {
+  modal.classList.remove("hide-modal");
 });
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.classList.add("hide-modal");
+  }
+};
 
+// Event listener for the <span> element that closes the modal
+$("#close-modal").on("click", function () {
+  modal.classList.add("hide-modal");
+});
+
+$("#delete-post-button").on("click", function () {
+  const postId = $(this).data("post-id");
+  $.ajax({
+    url: `/blog/${postId}`,
+    type: "DELETE",
+    success: function (result) {
+      alert("Post deleted successfully.");
+      // Optionally, you can redirect or remove the post from the DOM
+      window.location.href = "/";
+    },
+    error: function (err) {
+      alert("Error deleting post.");
+    },
+  }).then;
+});
