bd7e5e6fa4f6b69e74a1b473967e88f2404c7b61
Zacharias King <zacking1998@gmail.com>
2025-06-30 17:28:40 +0100

    Styling for Update and Delete post buttons and responsive design improved

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 const port = 3000;
 
   var testBlogPosts = [
-    { id: 1, imageName: 'Holi_FP_B118_1004194166.jpg',title: 'First Blog Post', content: 'This is the content of the first blog post.', date: '2023-10-01' },
-    { id: 2, imageName: 'Outdoor_99D_B022_100365573.jpg', title: 'Second Blog Post', content: 'This is the content of the second blog post.', date: '2024-10-01' },
-    { id: 3, imageName: 'smart home_110110747.jpg', title: 'Third Blog Post', content: 'This is the content of the third blog post.', date: '2024-10-23' }
+    { id: 1, imageName: 'Holi_FP_B118_1004194166.jpg',title: 'First Blog Post', content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum", date: '2023-10-01', author: 'John Doe' },
+    { id: 2, imageName: 'Outdoor_99D_B022_100365573.jpg', title: 'Second Blog Post', content: 'This is the content of the second blog post.', date: '2024-10-01', author: 'Jane Smith' },
+    { id: 3, imageName: 'smart home_110110747.jpg', title: 'Third Blog Post', content: 'This is the content of the third blog post.', date: '2024-10-23', author: 'Alice Johnson' }
   ];
 
 // Static files middleware
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 //middleware that only parses urlencoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -34,7 +39,9 @@ app.get('/contact', (req, res) => {
 app.get('/blog/:id', (req, res) => {
   const { id } = req.params;
   // Here you would typically fetch the blog post from a database
-  res.render('blog.ejs', { id });
+  const blogPost = testBlogPosts.find(post => post.id === parseInt(id));
+
+  res.render('blog.ejs', { blogPostForView: blogPost });
 });
 
 
